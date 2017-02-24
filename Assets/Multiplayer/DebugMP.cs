c19af8cf72d87d2062128291a4c88abb64b25246
rleinfellner <richard@modulo17.com>
2017-02-24 00:21:48 +0000

    Starting Multi player

---
+++ Assets/Multiplayer/DebugMP.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DebugMP : MonoBehaviour {
+
+	Text	mDebugtext;
+
+	// Use this for initialization
+	void Start () {
+		mDebugtext = GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		MovePlayer[] mGOs = FindObjectsOfType<MovePlayer> ();
+		mDebugtext.text = "";
+		foreach (var tP in mGOs) {
+			string	tType = (tP.isLocalPlayer) ? "Local" : "Remote";
+			mDebugtext.text += tP.name + tType + "\n";
+		}
+	}
+
+}
