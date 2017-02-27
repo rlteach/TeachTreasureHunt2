4528add1737feee9314e1e29fea517a47b8b01fa
rleinfellner <richard@modulo17.com>
2017-02-27 08:06:08 +0000

    Added isTarget to allow consistent Target check

--- Assets/Multiplayer/Scripts/Bullet.cs
+++ Assets/Multiplayer/Scripts/Bullet.cs
@@ -11,9 +11,15 @@ namespace Multiplayer {
 			}
 		}
 
+		bool	isTarget {		//Return true for valid bullet target
+			get {
+				return	(Type == EType.RemotePlayer || Type == EType.LocalPlayer);
+			}
+		}
+
 		protected override	void	CollidedWith(Entity vOther, bool vIsTrigger) {
 			base.CollidedWith (vOther, vIsTrigger);		//Print debug
-			if(vOther.Type==EType.RemotePlayer || vOther.Type==EType.LocalPlayer) {			//Bullets should only hit player
+			if(isTarget) {			//Bullets should only hit player
 				Player	tPlayer=(Player)vOther;		//We know its a player, so cast it
 				Destroy (gameObject);	//Kill bullet
 				tPlayer.TakeHit(1);		//Knock down player health
